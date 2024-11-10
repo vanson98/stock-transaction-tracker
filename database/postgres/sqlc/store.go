5dc44bf5016430195cd25596433e18f1e4566b78
vanson <nguyenvanson98123@gmail.com>
2024-11-10 15:53:27 +0700

    update store

--- database/postgres/sqlc/store.go
+++ database/postgres/sqlc/store.go
@@ -3,7 +3,6 @@ package db
 import (
 	"context"
 	"fmt"
-	"reflect"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -11,7 +10,7 @@ import (
 
 type IStore interface {
 	Querier
-	ExecTx(ctx context.Context, fn interface{}) (interface{}, error)
+	ExecTx(ctx context.Context, fn func(q *Queries) (interface{}, error)) (interface{}, error)
 }
 
 type DBStore struct {
@@ -26,35 +25,59 @@ func NewStore(cnnPool *pgxpool.Pool) IStore {
 	}
 }
 
-func (store *DBStore) ExecTx(ctx context.Context, fn interface{}) (interface{}, error) {
+func (store *DBStore) ExecTx(ctx context.Context, fn func(q *Queries) (interface{}, error)) (interface{}, error) {
 	tx, err := store.connectionPool.BeginTx(ctx, pgx.TxOptions{
 		IsoLevel: pgx.ReadCommitted,
 	})
 	if err != nil {
 		return nil, err
 	}
-	defer func() {
-		if err != nil {
-			if rbErr := tx.Rollback(ctx); rbErr != nil {
-				err = fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
-			}
-		} else {
-			err = tx.Commit(ctx)
+	result, err := fn(store.Queries)
+
+	if err != nil {
+		if rbErr := tx.Rollback(ctx); rbErr != nil {
+			err = fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
 		}
-	}()
-
-	fnValue := reflect.ValueOf(fn)
-	params := []reflect.Value{reflect.ValueOf(store.Queries)}
-	results := fnValue.Call(params)
-
-	if len(results) != 2 {
-		return nil, fmt.Errorf("fn must return (T, error)")
+	} else {
+		cmErr := tx.Commit(ctx)
+		if cmErr != nil {
+			err = fmt.Errorf("tx commit err : %v", cmErr)
+		}
 	}
 
-	if !results[1].IsNil() {
-		err = results[1].Interface().(error)
-		return nil, err
-	}
-
-	return results[0].Interface(), nil
+	return result, err
 }
+
+// func (store *DBStore) ExecTx(ctx context.Context, fn interface{}) (interface{}, error) {
+// 	tx, err := store.connectionPool.BeginTx(ctx, pgx.TxOptions{
+// 		IsoLevel: pgx.ReadCommitted,
+// 	})
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	fnValue := reflect.ValueOf(fn)
+// 	params := []reflect.Value{reflect.ValueOf(store.Queries)}
+// 	results := fnValue.Call(params)
+
+// 	if len(results) != 2 {
+// 		return nil, fmt.Errorf("fn must return (T, error)")
+// 	}
+
+// 	if !results[1].IsNil() {
+// 		err = results[1].Interface().(error)
+// 	}
+
+// 	if err != nil {
+// 		if rbErr := tx.Rollback(ctx); rbErr != nil {
+// 			err = fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
+// 		}
+// 	} else {
+// 		cmErr := tx.Commit(ctx)
+// 		if cmErr != nil {
+// 			err = fmt.Errorf("tx commit err : %v", cmErr)
+// 		}
+// 	}
+
+// 	return results[0].Interface(), err
+// }
