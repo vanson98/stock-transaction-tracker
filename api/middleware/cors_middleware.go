00e26eba7342fe5c9cb89a51c90213f0a4ebc244
vanson <nguyenvanson98123@gmail.com>
2025-02-02 16:54:51 +0700

    authen middleware

--- api/middleware/cors_middleware.go
+++ api/middleware/cors_middleware.go
@@ -1,10 +1,19 @@
 package middleware
 
 import (
+	"time"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
-func UseCors(engine *gin.Engine) {
-	engine.Use(cors.Default())
+func UseCors(router *gin.RouterGroup) {
+	corsMiddleware := cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:8081"},
+		AllowCredentials: true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Content-Type", "Authorization"},
+		MaxAge:           12 * time.Hour,
+	})
+	router.Use(corsMiddleware)
 }
--- api/middleware/jwt_auth_middleware.go
+++ api/middleware/jwt_auth_middleware.go
@@ -1,1 +1,36 @@
 package middleware
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UseTokenVerification(router *gin.RouterGroup) {
+	router.Use(func(ctx *gin.Context) {
+		authenCookie, err := ctx.Cookie("access_token")
+		if err != nil {
+			ctx.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		// make a verify token request to identity server
+		req, err := http.NewRequest("GET", "http://localhost:9091/token-verification", nil)
+		if err != nil {
+			ctx.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		req.Header.Set("Authorization", authenCookie)
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			ctx.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		if resp.StatusCode != http.StatusOK {
+			ctx.AbortWithStatus(resp.StatusCode)
+			return
+		}
+		defer resp.Body.Close()
+		ctx.Next()
+	})
+}
